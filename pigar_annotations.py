07b626296f8288d7f950eff04dea075a8181578d
andreyfesunov <fsnv04@gmail.com>
2025-04-20 00:20:14 +0300

    feat(docker): add dockerfiles, docker compose, fix reqs

--- pigar_annotations.py
+++ pigar_annotations.py
@@ -1,3 +1,5 @@
 # pigar: required-packages=aiosqlite
 # pigar: required-packages=pytest-asyncio
 # pigar: required-packages=duckduckgo-search
+# pigar: required-packages=llama-index
+# pigar: required-packages=llama-index-vector-stores-chroma
