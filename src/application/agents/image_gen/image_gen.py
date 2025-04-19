4186e75cf2e1d081347bd44734c8dd3b3879d41b
andrey_fesunov <fsnv04@gmail.com>
2025-04-19 14:20:06 +0300

    feat(agent): add image generator

---
+++ src/application/agents/image_gen/image_gen.py
@@ -0,0 +1,29 @@
+from pydantic_ai.models import Model
+from openai import OpenAI
+
+
+class ImageGenerator:
+    def __init__(self, llm: Model, api_key: str):
+        self.__llm = llm
+        self.__api_key = api_key
+        self.__client = self.__create_client()
+
+    def __create_client(self) -> OpenAI:
+        """
+        Create an client for image generation
+        """
+        return OpenAI(api_key=self.__api_key)
+
+    async def execute(self, image_prompt: str) -> str:
+        """
+        Generates image
+        """
+        result = self.__client.images.generate(
+            model=self.__llm,  # dall-e-3
+            prompt=image_prompt,
+            size="1024x1024",
+            quality="hd",  # standart 12 sec, hd 18 sec
+            n=1,
+            # response_format="b64_json" #url - url, b64_json ����� ���� �����
+        )
+        return result.data[0].url
